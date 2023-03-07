ceaed394d4804eac0b66d6308a7e36f85fa7382a
Naman-12911 <namangarg820@gmail.com>
2023-03-07 09:55:17 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0=30+$!^r#x&c6c9tra_0&&#)#u1&*!+nrfdavfe659qg58kba
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
